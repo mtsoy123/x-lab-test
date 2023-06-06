29193be488f3eb6ae6581e88b76f90bde81af142
Mikhail Tsoy <mixail0071@yandex.ru>
2023-06-06 13:36:46 +0200

    feature/add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import logo from "../../vendor/svg/logo.svg";
 
 type Props = {};
 
 function Header({}: Props) {
     return (
         <header className="header">
-            <img
-                src="../../vendor/svg/logo.svg"
-                alt="logo"
-                className="header__logo"
-            ></img>
-            <i className="icon icon_type_header-account"></i>
-            <p className="header__name-surname">Имя Фамилия</p>
+            <a href="#">
+                <img src={logo} alt="logo" className="header__logo" />
+            </a>
+            <a href="#" className="header__account-wrapper">
+                <i className="icon icon_type_header-account"></i>
+                <p className="header__name-surname">Имя Фамилия</p>
+            </a>
         </header>
     );
 }
